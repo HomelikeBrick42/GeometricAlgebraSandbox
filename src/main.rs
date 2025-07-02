89ea14ed41f687646846e7cdffda6fdc36b3d64c
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-07-02 18:50:30 +1200

    Added parameters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use crate::rendering::{GpuCamera, GpuQuad, RenderData, RenderState};
+use crate::{
+    multivector::Multivector,
+    rendering::{GpuCamera, GpuQuad, RenderData, RenderState},
+};
 use eframe::{egui, wgpu};
 
 pub mod multivector;
@@ -9,6 +12,8 @@ struct App {
     info_window_open: bool,
     camera_window_open: bool,
     camera: Camera,
+    parameters_window_open: bool,
+    parameters: Vec<Parameter>,
 }
 
 struct Camera {
@@ -18,6 +23,34 @@ struct Camera {
     zoom_speed: f32,
     show_grid: bool,
     grid_thickness: f32,
+}
+
+struct Parameter {
+    name: String,
+    type_: ParameterType,
+    color: cgmath::Vector3<f32>,
+    value: Multivector,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ParameterType {
+    Grade0,
+    Grade1,
+    Grade2,
+    Grade3,
+    Multivector,
+}
+
+impl ParameterType {
+    pub fn display_name(&self) -> &'static str {
+        match *self {
+            ParameterType::Grade0 => "Scalar",
+            ParameterType::Grade1 => "Grade 1",
+            ParameterType::Grade2 => "Grade 2",
+            ParameterType::Grade3 => "Grade 3",
+            ParameterType::Multivector => "Multivector",
+        }
+    }
 }
 
 impl App {
@@ -38,6 +71,8 @@ impl App {
                 show_grid: true,
                 grid_thickness: 0.05,
             },
+            parameters_window_open: true,
+            parameters: vec![],
         }
     }
 }
@@ -55,6 +90,7 @@ impl eframe::App for App {
             ui.horizontal(|ui| {
                 self.info_window_open |= ui.button("Info").clicked();
                 self.camera_window_open |= ui.button("Camera").clicked();
+                self.parameters_window_open |= ui.button("Parameters").clicked();
             });
         });
 
@@ -102,6 +138,141 @@ impl eframe::App for App {
                 ui.horizontal(|ui| {
                     ui.label("Grid Thickness:");
                     ui.add(egui::DragValue::new(&mut self.camera.grid_thickness).speed(0.01));
+                });
+            });
+
+        egui::Window::new("Parameters")
+            .open(&mut self.parameters_window_open)
+            .scroll([false, true])
+            .show(ctx, |ui| {
+                if ui.button("New Parameter").clicked() {
+                    self.parameters.push(Parameter {
+                        name: "Unnamed Parameter".into(),
+                        type_: ParameterType::Grade0,
+                        color: cgmath::Vector3 {
+                            x: 1.0,
+                            y: 0.0,
+                            z: 0.0,
+                        },
+                        value: Multivector::ZERO,
+                    });
+                }
+                let mut i = 0usize;
+                let mut delete = false;
+                self.parameters.retain_mut(|parameter| {
+                    egui::CollapsingHeader::new(&parameter.name)
+                        .id_salt(i)
+                        .show(ui, |ui| {
+                            ui.horizontal(|ui| {
+                                ui.label("Name:");
+                                ui.text_edit_singleline(&mut parameter.name);
+                            });
+
+                            ui.horizontal(|ui| {
+                                ui.label("Color:");
+                                ui.color_edit_button_rgb(parameter.color.as_mut());
+                            });
+
+                            ui.horizontal(|ui| {
+                                ui.label("Type:");
+                                if egui::ComboBox::from_id_salt("type")
+                                    .selected_text(parameter.type_.display_name())
+                                    .show_ui(ui, |ui| {
+                                        for type_ in [
+                                            ParameterType::Grade0,
+                                            ParameterType::Grade1,
+                                            ParameterType::Grade2,
+                                            ParameterType::Grade3,
+                                            ParameterType::Multivector,
+                                        ] {
+                                            ui.selectable_value(
+                                                &mut parameter.type_,
+                                                type_,
+                                                type_.display_name(),
+                                            );
+                                        }
+                                    })
+                                    .response
+                                    .changed()
+                                {
+                                    parameter.value = match parameter.type_ {
+                                        ParameterType::Grade0 => parameter.value.grade0(),
+                                        ParameterType::Grade1 => parameter.value.grade1(),
+                                        ParameterType::Grade2 => parameter.value.grade2(),
+                                        ParameterType::Grade3 => parameter.value.grade3(),
+                                        ParameterType::Multivector => parameter.value,
+                                    };
+                                }
+                            });
+
+                            let (grade0, grade1, grade2, grade3) = match parameter.type_ {
+                                ParameterType::Grade0 => (true, false, false, false),
+                                ParameterType::Grade1 => (false, true, false, false),
+                                ParameterType::Grade2 => (false, false, true, false),
+                                ParameterType::Grade3 => (false, false, false, true),
+                                ParameterType::Multivector => (true, true, true, true),
+                            };
+
+                            if grade0 {
+                                ui.horizontal(|ui| {
+                                    ui.label("Scalar:");
+                                    ui.add(egui::DragValue::new(&mut parameter.value.s).speed(0.1));
+                                });
+                            }
+                            if grade1 {
+                                ui.horizontal(|ui| {
+                                    ui.label("e0:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e0).speed(0.1),
+                                    );
+                                });
+                                ui.horizontal(|ui| {
+                                    ui.label("e1:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e1).speed(0.1),
+                                    );
+                                });
+                                ui.horizontal(|ui| {
+                                    ui.label("e2:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e2).speed(0.1),
+                                    );
+                                });
+                            }
+                            if grade2 {
+                                ui.horizontal(|ui| {
+                                    ui.label("e01:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e01).speed(0.1),
+                                    );
+                                });
+                                ui.horizontal(|ui| {
+                                    ui.label("e02:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e02).speed(0.1),
+                                    );
+                                });
+                                ui.horizontal(|ui| {
+                                    ui.label("e12:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e12).speed(0.1),
+                                    );
+                                });
+                            }
+                            if grade3 {
+                                ui.horizontal(|ui| {
+                                    ui.label("e012:");
+                                    ui.add(
+                                        egui::DragValue::new(&mut parameter.value.e012).speed(0.1),
+                                    );
+                                });
+                            }
+
+                            delete = ui.button("Delete").clicked();
+                        });
+
+                    i += 1;
+                    !delete
                 });
             });
 
